377e5d98e5073c852c37ab82e6fe4ddca588c507
Flamarion Jorge <29267749+flamarion@users.noreply.github.com>
2023-04-26 19:29:40 +0200

    chore: Docs for BYOB (#16)

--- examples/byob/main.tf
+++ examples/byob/main.tf
@@ -1,4 +1,21 @@
-moudle "byob" {
-  source = "../../modules/byob"
-  prefix = "test"
+provider "azurerm" {
+  features {}
+}
+
+module "byob" {
+  source              = "../../modules/byob"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  prefix              = var.prefix
+  deletion_protection = var.deletion_protection
+}
+
+
+output "blob_container" {
+  value = module.byob.blob_container
+}
+
+output "storage_key" {
+  value     = module.byob.azure_storage_key
+  sensitive = true
 }
