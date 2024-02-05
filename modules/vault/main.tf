cdafc4a9f2692ae11536ce50ff58f0b3cbce265d
George Scott <george.scott@wandb.com>
2024-02-05 14:38:44 -0600

    key permission changes

--- modules/vault/main.tf
+++ modules/vault/main.tf
@@ -29,9 +29,9 @@ resource "azurerm_key_vault_access_policy" "parent" {
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
 
-  key_permissions     = ["Get", "Backup", "Delete", "List", "Purge", "Recover", "Restore", "Rotate", "GetRotationPolicy"]
-  secret_permissions  = ["Get", "Backup", "Delete", "List", "Purge", "Recover", "Restore", "Set"]
-  storage_permissions = ["Get", "Backup", "Delete", "List", "Purge", "Recover", "Restore"]
+  key_permissions     = ["Backup", "Create", "Decrypt", "Delete", "Get", "GetRotationPolicy", "List", "Purge", "Recover", "Restore", "Rotate"]
+  secret_permissions  = ["Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"]
+  storage_permissions = ["Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore"]
 
   depends_on = [azurerm_key_vault.default]
 }
@@ -41,8 +41,8 @@ resource "azurerm_key_vault_access_policy" "identity" {
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = var.identity_object_id
 
-  key_permissions     = ["Get"]
-  secret_permissions  = ["Get", "Delete", "List", "Purge", "Recover", "Restore", "Set"]
+  key_permissions = ["Create", "Decrypt", "Get"]
+  secret_permissions = ["Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"]
   storage_permissions = ["Get"]
 
   depends_on = [azurerm_key_vault.default]
