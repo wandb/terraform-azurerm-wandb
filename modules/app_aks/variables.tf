25522dc6bbf86bef341a12f010d1fa393b72aa38
George Scott <george.scott@wandb.com>
2024-02-05 15:26:33 -0600

    Revert "alphabetize variables"
    This reverts commit d24d07be76cec99f204279aabf832177fb419708.

--- modules/app_aks/variables.tf
+++ modules/app_aks/variables.tf
@@ -1,3 +1,22 @@
+variable "namespace" {
+  type        = string
+  description = "Friendly name prefix used for tagging and naming Azure resources."
+}
+
+variable "identity" {
+  type = object({ id = string })
+}
+
+variable "resource_group" {
+  type        = object({ name = string, id = string })
+  description = "Resource Group where the Managed Kubernetes Cluster should exist."
+}
+
+variable "location" {
+  type        = string
+  description = "The location where the Managed Kubernetes Cluster should be created."
+}
+
 variable "cluster_subnet_id" {
   type        = string
   description = "Network subnet id for cluster"
@@ -7,27 +26,8 @@ variable "gateway" {
   type = object({ id = string })
 }
 
-variable "identity" {
-  type = object({ id = string })
-}
-
-variable "location" {
-  type        = string
-  description = "The location where the Managed Kubernetes Cluster should be created."
-}
-
-variable "namespace" {
-  type        = string
-  description = "Friendly name prefix used for tagging and naming Azure resources."
-}
-
 variable "public_subnet" {
   type = object({ id = string })
-}
-
-variable "resource_group" {
-  type        = object({ name = string, id = string })
-  description = "Resource Group where the Managed Kubernetes Cluster should exist."
 }
 
 variable "tags" {
