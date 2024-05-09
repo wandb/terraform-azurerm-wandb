9c98f3d6d1401055086cad1bc8b9feca68a6a45a
Andrew Levin <andrew.levin@wandb.com>
2024-05-09 09:14:41 -0700

    allow use of already existing Resource Group, allow user to pick whether or not to lock storage

--- modules/secure_storage_connector/variables.tf
+++ modules/secure_storage_connector/variables.tf
@@ -12,3 +12,20 @@ variable "oidc_issuer_url" {
   type        = string
   description = "OIDC Issuer URL from server deployment's AKS Cluster"
 }
+
+variable "deletion_protection" {
+  description = "If the instance should have deletion protection enabled. The storage container can't be deleted when this value is set to `true`."
+  type        = bool
+  default     = true
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = <<EOF
+The name of the resource group in which to create the managed identity and storage account.
+If not provided, a new resource group will be created and then managed by this terraform module.
+
+If you don't set this variable the first time you apply these changes, don't set it for later applies either.
+EOF
+  default     = ""
+}
