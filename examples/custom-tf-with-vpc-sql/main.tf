e0599495ee8109c483350ee66cbbce1b1ea59d25
amanpruthi <aman.pruthi@velotio.com>
2024-05-10 16:39:29 +0530

    fixed code

--- examples/custom-tf-with-vpc-sql/main.tf
+++ examples/custom-tf-with-vpc-sql/main.tf
@@ -61,18 +61,18 @@ module "vault" {
   tags = var.tags
 }
 
-# module "storage" {
-#   count  = (var.blob_container == "" && var.external_bucket == null) ? 1 : 0
-#   source = "../../modules/storage"
+module "storage" {
+  count  = (var.blob_container == "" && var.external_bucket == null) ? 1 : 0
+  source = "../../modules/storage"
 
-#   namespace           = var.namespace
-#   resource_group_name = local.resource_group.name.name
-#   location            = local.resource_group.location
-#   create_queue        = !var.use_internal_queue
-#   deletion_protection = var.deletion_protection
+  namespace           = var.namespace
+  resource_group_name = local.resource_group.name.name
+  location            = local.resource_group.location
+  create_queue        = !var.use_internal_queue
+  deletion_protection = var.deletion_protection
 
-#   tags = var.tags
-# }
+  tags = var.tags
+}
 
 module "app_lb" {
   source = "../../modules/app_lb"
@@ -104,133 +104,133 @@ module "app_aks" {
   tags = var.tags
 }
 
-# locals {
-#   container_name  = try(module.storage[0].container.name, "")
-#   account_name    = try(module.storage[0].account.name, "")
-#   access_key      = try(module.storage[0].account.primary_access_key, "")
-#   queue_name      = try(module.storage[0].queue.name, "")
-#   blob_container  = var.external_bucket == null ? coalesce(var.blob_container, local.container_name) : ""
-#   storage_account = var.external_bucket == null ? coalesce(var.storage_account, local.account_name) : ""
-#   storage_key     = var.external_bucket == null ? coalesce(var.storage_key, local.access_key) : ""
-#   bucket          = "az://${local.storage_account}/${local.blob_container}"
-#   queue           = (var.use_internal_queue || var.blob_container == "" || var.external_bucket == null) ? "internal://" : "az://${local.account_name}/${local.queue_name}"
+locals {
+  container_name  = try(module.storage[0].container.name, "")
+  account_name    = try(module.storage[0].account.name, "")
+  access_key      = try(module.storage[0].account.primary_access_key, "")
+  queue_name      = try(module.storage[0].queue.name, "")
+  blob_container  = var.external_bucket == null ? coalesce(var.blob_container, local.container_name) : ""
+  storage_account = var.external_bucket == null ? coalesce(var.storage_account, local.account_name) : ""
+  storage_key     = var.external_bucket == null ? coalesce(var.storage_key, local.access_key) : ""
+  bucket          = "az://${local.storage_account}/${local.blob_container}"
+  queue           = (var.use_internal_queue || var.blob_container == "" || var.external_bucket == null) ? "internal://" : "az://${local.account_name}/${local.queue_name}"
 
-#   redis_connection_string = "redis://:${module.redis.instance.primary_access_key}@${module.redis.instance.hostname}:${module.redis.instance.port}"
-# }
+  redis_connection_string = "redis://:${module.redis.instance.primary_access_key}@${module.redis.instance.hostname}:${module.redis.instance.port}"
+}
 
-# locals {
-#   service_account_name = "wandb-app"
-# }
+locals {
+  service_account_name = "wandb-app"
+}
 
-# resource "azurerm_federated_identity_credential" "app" {
-#   parent_id           = module.identity.identity.id
-#   name                = "${var.namespace}-app-credentials"
-#   resource_group_name = local.resource_group.name
-#   audience            = ["api://AzureADTokenExchange"]
-#   issuer              = module.app_aks.oidc_issuer_url
-#   subject             = "system:serviceaccount:default:${local.service_account_name}"
-# }
+resource "azurerm_federated_identity_credential" "app" {
+  parent_id           = module.identity.identity.id
+  name                = "${var.namespace}-app-credentials"
+  resource_group_name = local.resource_group.name
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = module.app_aks.oidc_issuer_url
+  subject             = "system:serviceaccount:default:${local.service_account_name}"
+}
 
-# module "cert_manager" {
-#   source    = "../../modules/cert_manager"
-#   namespace = var.namespace
+module "cert_manager" {
+  source    = "../../modules/cert_manager"
+  namespace = var.namespace
 
-#   ingress_class              = "azure/application-gateway"
-#   cert_manager_email         = "sysadmin@wandb.com"
-#   cert_manager_chart_version = "v1.9.1"
-#   tags                       = var.tags
+  ingress_class              = "azure/application-gateway"
+  cert_manager_email         = "sysadmin@wandb.com"
+  cert_manager_chart_version = "v1.9.1"
+  tags                       = var.tags
 
-#   depends_on = [module.app_aks]
-# }
+  depends_on = [module.app_aks]
+}
 
-# module "wandb" {
-#   source  = "wandb/wandb/helm"
-#   version = "1.2.0"
+module "wandb" {
+  source  = "wandb/wandb/helm"
+  version = "1.2.0"
 
-#   depends_on = [
-#     module.app_aks,
-#     module.cert_manager,
-#     module.database,
-#     module.storage,
-#   ]
-#   operator_chart_version = "1.1.2"
-#   controller_image_tag   = "1.10.1"
+  depends_on = [
+    module.app_aks,
+    module.cert_manager,
+    module.database,
+    module.storage,
+  ]
+  operator_chart_version = "1.1.2"
+  controller_image_tag   = "1.10.1"
 
-#   spec = {
-#     values = {
-#       global = {
-#         host    = local.url
-#         license = var.license
+  spec = {
+    values = {
+      global = {
+        host    = local.url
+        license = var.license
 
-#         bucket = var.external_bucket == null ? {
-#           provider  = "az"
-#           name      = local.storage_account
-#           path      = local.blob_container
-#           accessKey = local.storage_key
-#         } : var.external_bucket
+        bucket = var.external_bucket == null ? {
+          provider  = "az"
+          name      = local.storage_account
+          path      = local.blob_container
+          accessKey = local.storage_key
+        } : var.external_bucket
 
-#         mysql = {
-#           host     = var.database_env.host
-#           database = var.database_env.database_name
-#           user     = var.database_env.username
-#           password = var.database_env.password
-#           port     = 3306
-#         }
+        mysql = {
+          host     = var.database_env.host
+          database = var.database_env.database_name
+          user     = var.database_env.username
+          password = var.database_env.password
+          port     = 3306
+        }
 
-#         redis = {
-#           host     = module.redis.instance.hostname
-#           password = module.redis.instance.primary_access_key
-#           port     = module.redis.instance.port
-#         }
+        redis = {
+          host     = module.redis.instance.hostname
+          password = module.redis.instance.primary_access_key
+          port     = module.redis.instance.port
+        }
 
-#         extraEnv = var.other_wandb_env
+        extraEnv = var.other_wandb_env
 
-#       }
+      }
 
-#       app = {
-#         extraEnv = merge({
-#           "GORILLA_CUSTOMER_SECRET_STORE_AZ_CONFIG_VAULT_URI" = module.vault.vault.vault_uri,
-#           "GORILLA_CUSTOMER_SECRET_STORE_SOURCE"              = "az-secretmanager://wandb",
-#         }, var.app_wandb_env)
-#         pod = {
-#           labels = { "azure.workload.identity/use" = "true" }
-#         }
-#         serviceAccount = {
-#           name        = local.service_account_name
-#           annotations = { "azure.workload.identity/client-id" = module.identity.identity.client_id }
-#           labels      = { "azure.workload.identity/use" = "true" }
-#         }
-#       }
+      app = {
+        extraEnv = merge({
+          "GORILLA_CUSTOMER_SECRET_STORE_AZ_CONFIG_VAULT_URI" = module.vault.vault.vault_uri,
+          "GORILLA_CUSTOMER_SECRET_STORE_SOURCE"              = "az-secretmanager://wandb",
+        }, var.app_wandb_env)
+        pod = {
+          labels = { "azure.workload.identity/use" = "true" }
+        }
+        serviceAccount = {
+          name        = local.service_account_name
+          annotations = { "azure.workload.identity/client-id" = module.identity.identity.client_id }
+          labels      = { "azure.workload.identity/use" = "true" }
+        }
+      }
 
-#       ingress = {
-#         // TODO: For now we will use the existing issuer. We can move this into
-#         // the operator after testing. Trying to reduce the diff.
-#         issuer = { create = false }
+      ingress = {
+        // TODO: For now we will use the existing issuer. We can move this into
+        // the operator after testing. Trying to reduce the diff.
+        issuer = { create = false }
 
-#         annotations = {
-#           "kubernetes.io/ingress.class"         = "azure/application-gateway"
-#           "cert-manager.io/cluster-issuer"      = "cert-issuer"
-#           "cert-manager.io/acme-challenge-type" = "http01"
-#         }
+        annotations = {
+          "kubernetes.io/ingress.class"         = "azure/application-gateway"
+          "cert-manager.io/cluster-issuer"      = "cert-issuer"
+          "cert-manager.io/acme-challenge-type" = "http01"
+        }
 
-#         tls = [
-#           { hosts = [trimprefix(trimprefix(local.url, "https://"), "http://")], secretName = "wandb-ssl-cert" }
-#         ]
-#       }
+        tls = [
+          { hosts = [trimprefix(trimprefix(local.url, "https://"), "http://")], secretName = "wandb-ssl-cert" }
+        ]
+      }
 
-#       weave = {
-#         persistence = {
-#           provider = "azurefile"
-#         }
-#         extraEnv = var.weave_wandb_env
-#       }
+      weave = {
+        persistence = {
+          provider = "azurefile"
+        }
+        extraEnv = var.weave_wandb_env
+      }
 
-#       mysql = { install = false }
-#       redis = { install = false }
+      mysql = { install = false }
+      redis = { install = false }
 
-#       parquet = {
-#         extraEnv = var.parquet_wandb_env
-#       }
-#     }
-#   }
-# }
+      parquet = {
+        extraEnv = var.parquet_wandb_env
+      }
+    }
+  }
+}
