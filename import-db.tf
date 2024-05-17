6ec3735e0350c4a5f4d46699cc144b8760c39063
George Scott <george.scott@wandb.com>
2024-05-17 12:14:54 -0500

    remove import

--- import-db.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = "azurerm_mysql_flexible_server.default"
-  id = "/subscriptions/c213eb8e-d0e7-4bbb-985a-2f8deac5c1c5/resourceGroups/wandb-m-kopa/providers/Microsoft.DBforMySQL/flexibleServers/wandb-m-kopa-giving-quagga"
-}
-
-import {
-  to = "azurerm_mysql_flexible_database.default"
-  id = "/subscriptions/c213eb8e-d0e7-4bbb-985a-2f8deac5c1c5/resourceGroups/wandb-m-kopa/providers/Microsoft.DBforMySQL/flexibleServers/wandb-m-kopa-giving-quagga/databases/wandb_local"
-}
