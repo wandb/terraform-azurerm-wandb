4aeb5c28ae9c1d31a8aa65b38ea5a3da08adb5c6
Aman Pruthi <amanpruthi404@gmail.com>
2024-06-17 22:24:01 +0530

    feat: Added otel fix (#74)
    Co-authored-by: amanpruthi <aman.pruthi@velotio.com>

--- modules/identity/main.tf
+++ modules/identity/main.tf
@@ -3,3 +3,10 @@ resource "azurerm_user_assigned_identity" "default" {
   location            = var.location
   resource_group_name = var.resource_group.name
 }
+
+resource "azurerm_user_assigned_identity" "otel" {
+  count = var.otel_identity ? 1 : 0
+  name                = "${var.namespace}-identity-otel"
+  location            = var.location
+  resource_group_name = var.resource_group.name
+}
