0c053b8925ffcbd9bffddadd2829e26f16841558
Jeff Raubitschek <jeff@wandb.com>
2024-08-24 08:58:53 -0700

    force

--- modules/clickhouse/versions.tf
+++ modules/clickhouse/versions.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = "~> 1.0"
   required_providers {
     azapi = {
       source = "Azure/azapi"
-      version = "~> 1.11.0"
+      version = "~> 3.11.0"
     }
   }
 }
