f40fd978ce292c4c5729f3cbd2f749936a225811
Jeff Raubitschek <jeff@wandb.com>
2024-08-23 21:47:04 -0700

    try

--- modules/clickhouse/main.tf
+++ modules/clickhouse/main.tf
@@ -3,16 +3,12 @@ locals {
 }
 
 terraform {
-  required_version = "~> 1.0"
   required_providers {
     azapi = {
       source = "Azure/azapi"
       version = "~> 1.15.0"
     }
   }
-}
-
-provider "azapi" {
 }
 
 resource "azurerm_private_endpoint" "clickhouse" {
