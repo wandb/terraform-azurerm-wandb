d12da83d2b83d0b52cf5e0a1c5854233619ae2e7
Jeff Raubitschek <jeff@wandb.com>
2024-08-23 21:51:15 -0700

    update

--- main.tf
+++ main.tf
@@ -4,16 +4,6 @@ locals {
   url        = "${local.url_prefix}://${local.fqdn}"
 }
 
-terraform {
-  required_version = "~> 1.0"
-  required_providers {
-    azapi = {
-      source = "Azure/azapi"
-      version = "~> 1.15.0"
-    }
-  }
-}
-
 resource "azurerm_resource_group" "default" {
   name     = var.namespace
   location = var.location
