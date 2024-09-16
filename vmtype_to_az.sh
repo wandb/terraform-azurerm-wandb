6d9c0e68939719a846485f8e98fb94ad284cc08d
Daniel Panzella <daniel.panzella@wandb.com>
2024-09-16 15:01:57 -0700

    fix: allow specifying number of AZ to use when autoselecting zones

--- vmtype_to_az.sh
+++ vmtype_to_az.sh
@@ -11,16 +11,22 @@
 # Copy script arguments to named environment variables
 VM_TYPE="$1"
 REGION="$2"
+NUM_ZONES="$3"
 
 # Check if both arguments are provided
 if [ -z "$VM_TYPE" ] || [ -z "$REGION" ]; then
     echo "Error: Both VM type and region must be provided." >&2
-    echo "Usage: $0 <vm_type> <region>" >&2
+    echo "Usage: $0 <vm_type> <region> <num_zones>" >&2
     exit 1
 fi
 
+# Default to 3 zones if not specified
+if [ -z "$NUM_ZONES" ]; then
+    NUM_ZONES=3
+fi
+
 # Query Azure CLI for availability zones in the region for the specified VM type
-ZONES=$(az vm list-skus --location "$REGION" --size "$VM_TYPE" --query "[0].locationInfo[0].zones" -o json | jq -r -c 'sort | .[0:3]')
+ZONES=$(az vm list-skus --location "$REGION" --size "$VM_TYPE" --query "[0].locationInfo[0].zones" -o json | jq -r -c "sort | .[0:$NUM_ZONES]")
 
 # Check if the query returned any results
 if [ -z "$ZONES" ] || [ "$ZONES" == "null" ]; then
