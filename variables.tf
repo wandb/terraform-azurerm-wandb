ceb45552364f8afd94e523fa2983a750a71f85e7
Daniel Panzella <daniel.panzella@wandb.com>
2024-09-26 10:12:40 -0700

    fix: change default for node_pool_num_zones to keep the same number of zones by default

--- variables.tf
+++ variables.tf
@@ -220,8 +220,8 @@ variable "node_pool_zones" {
 
 variable "node_pool_num_zones" {
   type        = number
-  description = "Number of availability zones to use for the node pool when node_pool_zones is not set. If neither are set, 3 zones will be used"
-  default     = null
+  description = "Number of availability zones to use for the node pool when node_pool_zones is not set."
+  default     = 2
 }
 
 variable "node_max_pods" {
