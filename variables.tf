98fda68684fedd605e7573eae71ceded011f6240
Aditya Choudhari <aditya.choudhari@wandb.com>
2024-09-30 17:49:25 -0700

    add defaults

--- variables.tf
+++ variables.tf
@@ -93,11 +93,13 @@ variable "other_wandb_env" {
 variable "operator_chart_version" {
   type        = string
   description = "Version of the operator chart to deploy"
+  default     = "1.3.4"
 }
 
 variable "controller_image_tag" {
   type        = string
   description = "Tag of the controller image to deploy"
+  default     = "1.14.0"
 }
 
 
