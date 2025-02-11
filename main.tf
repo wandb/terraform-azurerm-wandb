4708682cfce13f64cd9170e33eb9eeafcfc01602
jonathan meeks <jonathan.meeks@wandb.com>
2025-02-10 18:45:12 -0600

    more redis tf fixesi

--- main.tf
+++ main.tf
@@ -318,9 +318,9 @@ module "wandb" {
           host = var.external_redis_host
           port = var.external_redis_port
           } : {
-          host     = var.create_redis ? module.redis.hostname : null
-          password = var.create_redis ? module.redis.primary_access_key : null
-          port     = var.create_redis ? module.redis.port : null
+          host     = var.create_redis ? module.redis[0].hostname : null
+          password = var.create_redis ? module.redis[0].primary_access_key : null
+          port     = var.create_redis ? module.redis[0].port : null
         }
 
         extraEnv = var.other_wandb_env
