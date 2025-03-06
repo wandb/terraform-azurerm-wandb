fddeb5d269c43a2330efbc9d30a4e6f0c18e3b70
Paul Yeo <paulyeo21@gmail.com>
2025-03-06 05:34:40 -0800

    fix: Revert Provision Azure storage container for SaaS BYOB (#118) (#126)

--- modules/secure_storage_connector/variables.tf
+++ modules/secure_storage_connector/variables.tf
@@ -5,8 +5,7 @@ variable "namespace" {
 
 variable "oidc_issuer_url" {
   type        = string
-  description = "OIDC Issuer URL"
-  default     = "https://accounts.google.com"
+  description = "OIDC Issuer URL from server deployment's AKS Cluster. Make sure to include the trailing '/'"
 }
 
 variable "deletion_protection" {
@@ -18,21 +17,4 @@ variable "deletion_protection" {
 variable "resource_group_name" {
   type        = string
   description = "The name of the resource group in which to create the managed identity and storage account."
-}
-
-variable "subscription_id" {
-  type    = string
-  default = null
-}
-
-variable "subject" {
-  type        = string
-  description = "The subject of the federated identity credential"
-  default     = "101148871499660386821"
-}
-
-variable "audience" {
-  type        = list(string)
-  description = "The audience of the federated identity credential"
-  default     = ["32555940559.apps.googleusercontent.com"]
 }
