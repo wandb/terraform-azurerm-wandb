d2dbe8838459db078aeacffa4406f61a14724878
Jonathan Meeks <jonathanmeeks@gmail.com>
2025-03-25 09:12:16 -0500

    fix: Bump k8s max nodes by 2

--- deployment-size.tf
+++ deployment-size.tf
@@ -4,35 +4,35 @@ locals {
     small = {
       db             = "MO_Standard_E2ds_v4",
       min_node_count = 1,
-      max_node_count = 2,
+      max_node_count = 4,
       node_instance  = "Standard_E4s_v5"
       cache          = "3"
     },
     medium = {
       db             = "MO_Standard_E4ds_v4",
       min_node_count = 1,
-      max_node_count = 2,
+      max_node_count = 4,
       node_instance  = "Standard_E4s_v5"
       cache          = "3"
     },
     large = {
       db             = "MO_Standard_E8ds_v4",
       min_node_count = 1,
-      max_node_count = 2,
+      max_node_count = 4,
       node_instance  = "Standard_E8s_v5"
       cache          = "4"
     },
     xlarge = {
       db             = "MO_Standard_E16ds_v4",
       min_node_count = 1,
-      max_node_count = 2,
+      max_node_count = 4,
       node_instance  = "Standard_E8s_v5"
       cache          = "4"
     },
     xxlarge = {
       db             = "MO_Standard_E32ds_v4",
       min_node_count = 1,
-      max_node_count = 3,
+      max_node_count = 5,
       node_instance  = "Standard_E16s_v5"
       cache          = "5"
     }
