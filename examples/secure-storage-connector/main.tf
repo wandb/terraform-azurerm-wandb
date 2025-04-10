5787b126ca04f42e8f295245685fcf90457ab9a7
Andrew Levin <andrew.levin@wandb.com>
2025-04-10 15:52:30 -0700

    fix: Use vars in secure_storage_connector examples

--- examples/secure-storage-connector/main.tf
+++ examples/secure-storage-connector/main.tf
@@ -1,17 +1,17 @@
 provider "azurerm" {
   features {}
-  subscription_id = "26c54111-ca63-4feb-a59e-e70beb7f72eb"
-  tenant_id       = "df6b81d4-a363-4352-b652-f7e584129264"
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
 
 
 
 module "secure_storage_connector" {
   source              = "../../modules/secure_storage_connector"
-  namespace           = "zacharyblasczyk"
-  resource_group_name = "zacharyblasczyk"
-  oidc_issuer_url     = "https://eastus.oic.prod-aks.azure.com/af722783-84b6-4adc-9c49-c792786eab4a/ba51b00b-03ae-4850-a8c4-beaa5f0bd79e/"
-  deletion_protection = false
+  namespace           = var.namespace
+  resource_group_name = var.resource_group_name
+  oidc_issuer_url     = var.oidc_issuer_url
+  deletion_protection = var.deletion_protection
 }
 
 output "storage_account_name" {
