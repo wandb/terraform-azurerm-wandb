bca22f65597e0a31f3fce929fe571e274684e7f1
Daniel Barnes <dabarnes2b@gmail.com>
2025-06-25 04:57:25 +0900

    wire through dns resolver to ingress annotations

--- modules/app_ingress/variables.tf
+++ modules/app_ingress/variables.tf
@@ -6,3 +6,9 @@ variable "fqdn" {
 variable "namespace" {
   type = string
 }
+
+variable "use_dns_challenge" {
+  type        = bool
+  description = "Should be used in conjuction with the cert_manager's modules variable"
+  default     = false
+}
