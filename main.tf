08a66636da8aba422e84874cf361a5d3f3e1b0a2
Daniel Panzella <daniel.panzella@wandb.com>
2025-06-30 15:36:46 -0700

    fix: Ignore changes to the application gw zone config. (#140)

--- main.tf
+++ main.tf
@@ -271,6 +271,11 @@ module "cert_manager" {
   depends_on = [module.app_aks]
 }
 
+moved {
+  from = module.cert_manager
+  to   = module.cert_manager[0]
+}
+
 module "clickhouse" {
   count               = var.clickhouse_private_endpoint_service_name != "" ? 1 : 0
   source              = "./modules/clickhouse"
