6e11e9b62b6fa07d79d643a7934a804f29e1403c
Jonathan Meeks <jonathan.meeks@wandb.com>
2025-09-04 10:45:59 -0500

    feat: Add option for chainguard redis (#154)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -159,7 +159,7 @@ module "app_aks" {
   public_subnet           = module.networking.public_subnet
   resource_group          = azurerm_resource_group.default
   sku_tier                = var.cluster_sku_tier
-  tags                    = merge(var.tags, { cache_size = var.cache_size }, var.kubernetes_cluster_tags)
+  tags                    = merge(var.tags, var.kubernetes_cluster_tags)
 }
 locals {
   service_account_name         = "wandb-app"
@@ -310,6 +310,11 @@ locals {
   ctrlplane_redis_params = {
     master = "gorilla"
   }
+  chainguard_redis_host = "redis.redis-cg.svc.cluster.local"
+  chainguard_redis_port = "26379"
+  chainguard_redis_params = {
+    master = "gorilla"
+  }
   spec = {
     values = {
       global = {
@@ -343,6 +348,12 @@ locals {
           port     = local.ctrlplane_redis_port
           params   = local.ctrlplane_redis_params
           external = true
+          } : var.use_chainguard_redis ? {
+          host     = local.chainguard_redis_host
+          password = ""
+          port     = local.chainguard_redis_port
+          params   = local.chainguard_redis_params
+          external = true
           } : var.use_external_redis ? {
           host     = var.external_redis_host
           password = ""
@@ -370,8 +381,8 @@ locals {
         extraEnv = merge({
           "GORILLA_CUSTOMER_SECRET_STORE_AZ_CONFIG_VAULT_URI" = module.vault.vault.vault_uri,
           "GORILLA_CUSTOMER_SECRET_STORE_SOURCE"              = "az-secretmanager://wandb",
-          "GORILLA_SECRET_STORE_AZ_CONFIG_VAULT_URI" = module.vault.vault.vault_uri,
-          "GORILLA_SECRET_STORE_SOURCE"              = "az-secretmanager://wandb",
+          "GORILLA_SECRET_STORE_AZ_CONFIG_VAULT_URI"          = module.vault.vault.vault_uri,
+          "GORILLA_SECRET_STORE_SOURCE"                       = "az-secretmanager://wandb",
         }, var.other_wandb_env)
       }
 
@@ -432,8 +443,8 @@ locals {
         }
 
         labels = var.create_private_link ? {
-            "sha_hash" = substr(sha256("yes"), 0, 50)
-          } : {}
+          "sha_hash" = substr(sha256("yes"), 0, 50)
+        } : {}
 
         tls = [
           { hosts = [trimprefix(trimprefix(local.url, "https://"), "http://")], secretName = "wandb-ssl-cert" }
@@ -443,7 +454,7 @@ locals {
           create       = var.create_private_link
           nameOverride = "${var.namespace}-private-ingress"
           annotations = {
-            "kubernetes.io/ingress.class"                   = "azure/application-gateway"
+            "kubernetes.io/ingress.class"                 = "azure/application-gateway"
             "appgw.ingress.kubernetes.io/request-timeout" = "300"
             "appgw.ingress.kubernetes.io/use-private-ip" : "true"
           }
@@ -452,7 +463,7 @@ locals {
           ]
         }
       }
- 
+
 
       otel = {
         daemonset = var.azuremonitor ? {
@@ -626,4 +637,19 @@ module "wandb" {
   controller_image_tag   = var.controller_image_tag
   enable_helm_operator   = var.enable_helm_operator
   enable_helm_wandb      = var.enable_helm_wandb
+}
+
+resource "null_resource" "use_redis_validation" {
+  triggers = {
+    use_ctrlplane_redis  = var.use_ctrlplane_redis
+    use_chainguard_redis = var.use_chainguard_redis
+    use_external_redis   = var.use_external_redis
+  }
+
+  lifecycle {
+    precondition {
+      condition     = (var.use_ctrlplane_redis ? 1 : 0) + (var.use_chainguard_redis ? 1 : 0) + (var.use_external_redis ? 1 : 0) <= 1
+      error_message = "Enable at most one of: use_ctrlplane_redis, use_chainguard_redis, use_external_redis."
+    }
+  }
 }
