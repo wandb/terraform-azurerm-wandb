05a8c85bc1c74f76d7afe13fe7133deea651ae0b
Aravind Warrier <awarrier@wandb.com>
2025-10-29 12:34:40 -0400

    Add CSI driver version variables to root module
    These variables are required by the root module to fetch the Azure provider
    manifest and pass configuration to the app_aks module.

--- variables.tf
+++ variables.tf
@@ -410,3 +410,18 @@ variable "k8s_namespace" {
   description = "The Kubernetes namespace where W&B resources will be deployed"
   default     = "default"
 }
+
+###########################################
+# Secrets Store CSI Driver                #
+###########################################
+variable "secrets_store_csi_driver_version" {
+  type        = string
+  description = "The version of the Secrets Store CSI Driver Helm chart to install"
+  default     = "1.4.7"
+}
+
+variable "secrets_store_csi_driver_provider_azure_version" {
+  type        = string
+  description = "The version of the Azure Key Vault Provider for Secrets Store CSI Driver to install"
+  default     = "1.7.1"
+}
