25dbd5eaad2a4fb4a326f6ca60ffe38be69759df
Aravind Warrier <awarrier@wandb.com>
2025-10-29 20:49:01 -0400

    Remove temporary bufstream testing outputs

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -9,10 +9,3 @@ output "container" {
 output "queue" {
   value = var.create_queue ? module.queue.0.queue : null
 }
-
-# TODO(aravind): Remove - for testing bufstream
-output "primary_access_key" {
-  value       = azurerm_storage_account.default.primary_access_key
-  sensitive   = true
-  description = "Primary access key for storage account"
-}
