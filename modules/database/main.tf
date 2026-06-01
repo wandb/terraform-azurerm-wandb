149d0f1e967c9255037a7c682bfa2aa8a8491306
Aditya Choudhari <48932219+adityachoudhari26@users.noreply.github.com>
2026-06-01 11:54:54 -0400

    Update modules/database/main.tf
    Co-authored-by: jlg-wandb <jleonguerrero@wandb.com>

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -22,16 +22,11 @@ locals {
   default_flags = {
     "binlog_row_image"           = "minimal"
     "max_binlog_size"            = "1073741824"
-    "innodb_autoinc_lock_mode"   = "2"
-    "innodb_lru_scan_depth"      = "100"
-    "innodb_print_all_deadlocks" = "off"
-    "local_infile"               = "on"
-    "long_query_time"            = "1"
-    "max_prepared_stmt_count"    = "1048576"
+    "long_query_time"            = "4"
+    "max_prepared_stmt_count"    = "65528"
     "max_execution_time"         = "60000"
     "slow_query_log"             = "on"
     "sort_buffer_size"           = tostring(var.sort_buffer_size)
-    "skip_show_database"         = "on"
   }
   database_flags = merge(local.default_flags, var.database_flags)
 }
